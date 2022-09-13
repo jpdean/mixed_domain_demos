809b5615eccf3b3a0d979876089044c83197965a
Joe Dean <jpd62@cam.ac.uk>
2022-09-13 16:24:48 +0100

    Add exact solution

--- poisson_neumann_bc.py
+++ poisson_neumann_bc.py
@@ -1,9 +1,10 @@
 import numpy as np
 import ufl
 from dolfinx import fem, io, mesh
-from ufl import grad, inner
+from ufl import grad, inner, div, dot
 from mpi4py import MPI
 from petsc4py import PETSc
+from utils import norm_L2
 
 
 def boundary_marker(x):
@@ -13,41 +14,44 @@ def boundary_marker(x):
                                        np.isclose(x[1], 1.0)))
 
 
-n = 4
+# Create mesh and submesh of boundary
+n = 8
 msh = mesh.create_unit_square(MPI.COMM_WORLD, n, n)
-fdim = msh.topology.dim - 1
+tdim = msh.topology.dim
+fdim = tdim - 1
 num_facets = msh.topology.create_entities(fdim)
 boundary_facets = mesh.locate_entities_boundary(msh, fdim, boundary_marker)
 submesh, entity_map = mesh.create_submesh(msh, fdim, boundary_facets)[0:2]
 
-V = fem.FunctionSpace(msh, ("Lagrange", 1))
-W = fem.FunctionSpace(submesh, ("Lagrange", 1))
+# Create function spaces
+k = 1
+V = fem.FunctionSpace(msh, ("Lagrange", k))
+W = fem.FunctionSpace(submesh, ("Lagrange", k))
 u = ufl.TrialFunction(V)
 v = ufl.TestFunction(V)
 
+# Create integration measure and entity maps
 ds = ufl.Measure("ds", domain=msh)
-
-
-# with io.XDMFFile(submesh.comm, "g.xdmf", "w") as file:
-#     file.write_mesh(submesh)
-#     file.write_function(g)
-
 entity_maps = {submesh: [entity_map.index(entity)
                          if entity in entity_map else -1
                          for entity in range(num_facets)]}
 
-a = fem.form(inner(u, v) * ufl.dx + inner(grad(u), grad(v)) * ufl.dx)
-# FIXME Problem packing coefficients for both cell and facet space in one form
-# f = fem.Function(V)
-# f.interpolate(lambda x: np.ones_like(x[0]))
-g = fem.Function(W)
-g.interpolate(lambda x: x[0])
-# L = fem.form(inner(f, v) * ufl.dx + inner(g, v) * ds, entity_maps=entity_maps)
-L = fem.form(inner(g, v) * ds, entity_maps=entity_maps)
+# Create manufactured solution
+x = ufl.SpatialCoordinate(msh)
+u_e = 1
+for i in range(tdim):
+    u_e *= ufl.sin(ufl.pi * x[i])
+f = u_e - div(grad(u_e))
+n = ufl.FacetNormal(msh)
+g = dot(grad(u_e), n)
 
+# Define forms
+a = fem.form(inner(u, v) * ufl.dx + inner(grad(u), grad(v)) * ufl.dx)
+L = fem.form(inner(f, v) * ufl.dx + inner(g, v) * ds, entity_maps=entity_maps)
+
+# Assemble matrix and vector
 A = fem.petsc.assemble_matrix(a)
 A.assemble()
-
 b = fem.petsc.assemble_vector(L)
 b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
 
@@ -66,3 +70,8 @@ u.x.scatter_forward()
 # Write to file
 with io.VTXWriter(msh.comm, "u.bp", u) as f:
     f.write(0.0)
+
+e_L2 = norm_L2(msh.comm, u - u_e)
+
+if msh.comm.rank == 0:
+    print(f"e_L2 = {e_L2}")
