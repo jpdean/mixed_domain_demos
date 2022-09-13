04051cd3e63cc8a070bcbed6e3f53a58f55f1735
Joe Dean <jpd62@cam.ac.uk>
2022-09-13 10:32:46 +0100

    Start refactoring

--- lagrange_multiplier.py
+++ lagrange_multiplier.py
@@ -7,55 +7,16 @@ from petsc4py import PETSc
 import random
 
 
-def create_random_mesh(corners, n, ghost_mode):
-    """Create a rectangular mesh made of randomly ordered simplices"""
-    if MPI.COMM_WORLD.rank == 0:
-        h_x = (corners[1][0] - corners[0][0]) / n[0]
-        h_y = (corners[1][1] - corners[0][1]) / n[1]
-
-        points = [(i * h_x, j * h_y)
-                  for i in range(n[0] + 1) for j in range(n[1] + 1)]
-
-        random.seed(6)
-
-        cells = []
-        for i in range(n[0]):
-            for j in range(n[1]):
-                v = (n[1] + 1) * i + j
-                cell_0 = [v, v + 1, v + n[1] + 2]
-                random.shuffle(cell_0)
-                cells.append(cell_0)
-
-                cell_1 = [v, v + n[1] + 1, v + n[1] + 2]
-                random.shuffle(cell_1)
-                cells.append(cell_1)
-        cells = np.array(cells)
-        points = np.array(points)
-    else:
-        cells, points = np.empty([0, 3]), np.empty([0, 2])
-
-    domain = ufl.Mesh(ufl.VectorElement("Lagrange", "triangle", 1))
-    partitioner = mesh.create_cell_partitioner(ghost_mode)
-    return mesh.create_mesh(MPI.COMM_WORLD, cells, points, domain,
-                            partitioner=partitioner)
-
-
 def norm_L2(comm, v):
     return np.sqrt(comm.allreduce(fem.assemble_scalar(
         fem.form(inner(v, v) * ufl.dx)), op=MPI.SUM))
 
 
-n = 16
+n = 8
 assert n % 2 == 0  # NOTE n must be even
 k = 1
-# msh = mesh.create_unit_square(MPI.COMM_WORLD, n, n)
-msh = create_random_mesh(((0.0, 0.0), (1.0, 1.0)), (n, n),
-                         mesh.GhostMode.shared_facet)
+msh = mesh.create_unit_square(MPI.COMM_WORLD, n, n)
 # msh = mesh.create_unit_cube(MPI.COMM_WORLD, n, n, n)
-
-
-with io.XDMFFile(msh.comm, "msh.xdmf", "w") as file:
-    file.write_mesh(msh)
 
 V = fem.FunctionSpace(msh, ("Lagrange", k))
 u = ufl.TrialFunction(V)
@@ -63,11 +24,11 @@ v = ufl.TestFunction(V)
 
 # Create Dirichlet boundary condition
 tdim = msh.topology.dim
-facet_dim = tdim - 1
+fdim = tdim - 1
 dirichlet_facets = mesh.locate_entities_boundary(
-    msh, facet_dim, lambda x: np.logical_or(np.isclose(x[0], 0.0),
+    msh, fdim, lambda x: np.logical_or(np.isclose(x[0], 0.0),
                                             np.isclose(x[0], 1.0)))
-dirichlet_dofs = fem.locate_dofs_topological(V, facet_dim, dirichlet_facets)
+dirichlet_dofs = fem.locate_dofs_topological(V, fdim, dirichlet_facets)
 bc = fem.dirichletbc(PETSc.ScalarType(0.0), dirichlet_dofs, V)
 
 # FIXME Need to use this clumsy method until we have better support for one
@@ -84,9 +45,9 @@ with io.XDMFFile(submesh_left_cells.comm, "submesh_left_cells.xdmf", "w") as fil
 # the centre facets of the original mesh
 # NOTE Numbered with respect to submesh_left_cells
 centre_facets = mesh.locate_entities_boundary(
-    submesh_left_cells, facet_dim, lambda x: np.isclose(x[0], 0.5))
+    submesh_left_cells, fdim, lambda x: np.isclose(x[0], 0.5))
 mt = mesh.meshtags(
-    submesh_left_cells, facet_dim, centre_facets, 1)
+    submesh_left_cells, fdim, centre_facets, 1)
 
 # Create an exterior facet measure on the submesh using the meshtags. Forms
 # with ds(1) therefore integrates over the centre facets of the orignial mesh
@@ -95,14 +56,14 @@ ds = ufl.Measure("ds", domain=submesh_left_cells, subdomain_data=mt)
 # Create a submesh of the centre facets of the mesh to define the function space
 # for the Lagrange multiplier
 submesh_centre_facets, entity_map_centre_facets = mesh.create_submesh(
-    submesh_left_cells, facet_dim, centre_facets)[0:2]
+    submesh_left_cells, fdim, centre_facets)[0:2]
 with io.XDMFFile(submesh_centre_facets.comm,
                  "submesh_centre_facets.xdmf", "w") as file:
     file.write_mesh(submesh_centre_facets)
 
 # We need to provide entitiy maps for both the original mesh and the submesh
 # of the centre facets
-facet_imap = submesh_left_cells.topology.index_map(facet_dim)
+facet_imap = submesh_left_cells.topology.index_map(fdim)
 sm_lc_num_facets = facet_imap.size_local + facet_imap.num_ghosts
 entity_maps = {msh: entity_map_left_cells,
                submesh_centre_facets: [entity_map_centre_facets.index(entity)
