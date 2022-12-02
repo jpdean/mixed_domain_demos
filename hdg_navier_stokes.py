be47ff8c921728f8d650cb56212538a335bab6d6
Joe Dean <jpd62@cam.ac.uk>
2022-12-02 15:00:48 +0000

    Don't redefine n

--- hdg_navier_stokes.py
+++ hdg_navier_stokes.py
@@ -44,7 +44,6 @@ comm = MPI.COMM_WORLD
 rank = comm.rank
 out_str = f"rank {rank}:\n"
 
-n = 8
 # msh = mesh.create_unit_square(
 #     comm, n, n, ghost_mode=mesh.GhostMode.none,
 #     cell_type=mesh.CellType.quadrilateral)
