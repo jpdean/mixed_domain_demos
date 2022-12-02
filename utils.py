c48d6a21e44708ae987ed0021eb3260c0abcdaf5
Joe Dean <jpd62@cam.ac.uk>
2022-12-02 10:39:53 +0000

    Remove reorder

--- utils.py
+++ utils.py
@@ -7,29 +7,6 @@ from dolfinx.cpp.mesh import cell_num_entities
 from dolfinx import mesh
 
 
-def reorder_mesh(msh):
-    # FIXME Check this is correct
-    # FIXME For a high-order mesh, the geom has more dofs so need to modify
-    # this
-    # FIXME What about quads / hexes?
-    tdim = msh.topology.dim
-    num_cell_vertices = cell_num_entities(msh.topology.cell_type, 0)
-    c_to_v = msh.topology.connectivity(tdim, 0)
-    geom_dofmap = msh.geometry.dofmap
-    vertex_imap = msh.topology.index_map(0)
-    geom_imap = msh.geometry.index_map()
-    for i in range(0, len(c_to_v.array), num_cell_vertices):
-        topo_perm = np.argsort(vertex_imap.local_to_global(
-            c_to_v.array[i:i+num_cell_vertices]))
-        geom_perm = np.argsort(geom_imap.local_to_global(
-            geom_dofmap.array[i:i+num_cell_vertices]))
-
-        c_to_v.array[i:i+num_cell_vertices] = \
-            c_to_v.array[i:i+num_cell_vertices][topo_perm]
-        geom_dofmap.array[i:i+num_cell_vertices] = \
-            geom_dofmap.array[i:i+num_cell_vertices][geom_perm]
-
-
 def norm_L2(comm, v, measure=ufl.dx):
     return np.sqrt(comm.allreduce(fem.assemble_scalar(
         fem.form(ufl.inner(v, v) * measure)), op=MPI.SUM))
@@ -86,3 +63,39 @@ def convert_facet_tags(msh, submesh, cell_map, facet_tag):
     submesh_meshtags = mesh.meshtags(
         submesh, submesh.topology.dim - 1, submesh_facets, submesh_values)
     return submesh_meshtags
+
+
+def create_random_mesh(corners, n, ghost_mode):
+    """Create a rectangular mesh made of randomly ordered simplices"""
+    if MPI.COMM_WORLD.rank == 0:
+        h_x = (corners[1][0] - corners[0][0]) / n[0]
+        h_y = (corners[1][1] - corners[0][1]) / n[1]
+
+        points = [(i * h_x, j * h_y)
+                  for i in range(n[0] + 1) for j in range(n[1] + 1)]
+
+        import random
+        random.seed(6)
+
+        cells = []
+        for i in range(n[0]):
+            for j in range(n[1]):
+                v = (n[1] + 1) * i + j
+                cell_0 = [v, v + 1, v + n[1] + 2]
+                random.shuffle(cell_0)
+                cells.append(cell_0)
+
+                cell_1 = [v, v + n[1] + 1, v + n[1] + 2]
+                random.shuffle(cell_1)
+                cells.append(cell_1)
+        cells = np.array(cells)
+        points = np.array(points)
+    else:
+        cells, points = np.empty([0, 3]), np.empty([0, 2])
+
+    import basix.ufl_wrapper
+    domain = ufl.Mesh(basix.ufl_wrapper.create_vector_element(
+        "Lagrange", "triangle", 1))
+    partitioner = mesh.create_cell_partitioner(ghost_mode)
+    return mesh.create_mesh(MPI.COMM_WORLD, cells, points, domain,
+                            partitioner=partitioner)
