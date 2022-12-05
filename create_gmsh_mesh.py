122a0cfb4ba3e495793992c6907e5cc1ee41b213
Joe Dean <jpd62@cam.ac.uk>
2022-12-05 17:00:11 +0000

    Generalise

--- create_gmsh_mesh.py
+++ create_gmsh_mesh.py
@@ -12,22 +12,24 @@ def create():
     gmsh.initialize()
     if comm.rank == 0:
         gmsh.model.add("unit_square")
-        lc = 1e-1
-
-        # num_bottom_points = 10
-        # xs = np.linspace(0.0, 3.0, num_bottom_points)
+        lc = 0.1
 
         # Point tags
-        points = [gmsh.model.geo.addPoint(0, 0, 0, lc),
-                  gmsh.model.geo.addPoint(3, 0, 0, lc),
-                  gmsh.model.geo.addPoint(3, 1, 0, lc),
-                  gmsh.model.geo.addPoint(0, 1, 0, lc)]
+        num_bottom_points = 100
+        bottom_points = [gmsh.model.geo.addPoint(x, 0, 0, lc)
+                         for x in np.linspace(0.0, 3.0, num_bottom_points)]
+        top_left_point = gmsh.model.geo.addPoint(0, 1, 0, lc)
+        top_right_point = gmsh.model.geo.addPoint(3, 1, 0, lc)
 
         # Line tags
         lines = []
-        for i in range(len(points) - 1):
-            lines.append(gmsh.model.geo.addLine(points[i], points[i + 1]))
-        lines.append(gmsh.model.geo.addLine(points[-1], points[0]))
+        lines.append(gmsh.model.geo.addSpline(bottom_points))
+        lines.append(gmsh.model.geo.addLine(bottom_points[-1],
+                                            top_right_point))
+        lines.append(gmsh.model.geo.addLine(top_right_point,
+                                            top_left_point))
+        lines.append(gmsh.model.geo.addLine(top_left_point,
+                                            bottom_points[0]))
 
         gmsh.model.geo.addCurveLoop(lines, 1)
 
@@ -37,13 +39,14 @@ def create():
 
         gmsh.model.addPhysicalGroup(2, [1], 1)
 
-        gmsh.model.addPhysicalGroup(1, [1], 1)
-        gmsh.model.addPhysicalGroup(1, [2], 2)
-        gmsh.model.addPhysicalGroup(1, [3], 3)
-        gmsh.model.addPhysicalGroup(1, [4], 4)
+        gmsh.model.addPhysicalGroup(1, [lines[0]], 1)
+        gmsh.model.addPhysicalGroup(1, [lines[1]], 2)
+        gmsh.model.addPhysicalGroup(1, [lines[2]], 3)
+        gmsh.model.addPhysicalGroup(1, [lines[3]], 4)
 
         gmsh.option.setNumber("Mesh.RecombineAll", 1)
         gmsh.option.setNumber("Mesh.Algorithm", 8)
+        gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
         gmsh.model.mesh.generate(2)
 
         # gmsh.write("t1.msh")
