c83c805bd84a261989685806d1ce6bde257ab0c2
Joe Dean <jpd62@cam.ac.uk>
2022-12-06 12:15:57 +0000

    Move mesh creation

--- hdg_navier_stokes.py
+++ hdg_navier_stokes.py
@@ -1,4 +1,3 @@
-import create_gmsh_mesh
 from dolfinx import mesh, fem, io
 from mpi4py import MPI
 import ufl
@@ -9,6 +8,8 @@ from dolfinx.cpp.mesh import cell_num_entities
 from dolfinx.cpp.fem import compute_integration_domains
 from utils import norm_L2, domain_average, normal_jump_error
 from enum import Enum
+import gmsh
+from dolfinx.io import gmshio
 
 
 class SolverType(Enum):
@@ -272,8 +273,73 @@ class Problem:
 
 class GaussianBump(Problem):
     def create_mesh(self):
-        # TODO Move mesh creation code here
-        msh, mt = create_gmsh_mesh.create()
+        def gaussian(x, a, sigma, mu):
+            return a * np.exp(- 1 / 2 * ((x - mu) / sigma)**2)
+
+        comm = MPI.COMM_WORLD
+        gdim = 2
+
+        gmsh.initialize()
+        if comm.rank == 0:
+            # TODO Pass options
+            gmsh.model.add("gaussian_bump")
+            lc = 0.1
+            a = 0.2
+            sigma = 0.2
+            mu = 1.0
+            w = 5.0
+            order = 1
+            num_bottom_points = 100
+
+            # Point tags
+            bottom_points = [
+                gmsh.model.geo.addPoint(x, gaussian(x, a, sigma, mu), 0.0, lc)
+                for x in np.linspace(0.0, w, num_bottom_points)]
+            top_left_point = gmsh.model.geo.addPoint(0, 1, 0, lc)
+            top_right_point = gmsh.model.geo.addPoint(w, 1, 0, lc)
+
+            # Line tags
+            lines = []
+            lines.append(gmsh.model.geo.addSpline(bottom_points))
+            lines.append(gmsh.model.geo.addLine(bottom_points[-1],
+                                                top_right_point))
+            lines.append(gmsh.model.geo.addLine(top_right_point,
+                                                top_left_point))
+            lines.append(gmsh.model.geo.addLine(top_left_point,
+                                                bottom_points[0]))
+
+            gmsh.model.geo.addCurveLoop(lines, 1)
+
+            gmsh.model.geo.addPlaneSurface([1], 1)
+
+            # gmsh.model.geo.mesh.setTransfiniteCurve(1, 40)
+            # gmsh.model.geo.mesh.setTransfiniteCurve(2, 15, "Progression", 1.1)
+            # gmsh.model.geo.mesh.setTransfiniteCurve(3, 40)
+            # gmsh.model.geo.mesh.setTransfiniteCurve(4, 15, "Progression", -1.1)
+            # gmsh.model.geo.mesh.setTransfiniteSurface(
+            #     1, "Left", [bottom_points[0], bottom_points[-1], top_right_point, top_left_point])
+
+            gmsh.model.geo.synchronize()
+
+            gmsh.model.addPhysicalGroup(2, [1], 1)
+
+            gmsh.model.addPhysicalGroup(1, [lines[0]], 1)
+            gmsh.model.addPhysicalGroup(1, [lines[1]], 2)
+            gmsh.model.addPhysicalGroup(1, [lines[2]], 3)
+            gmsh.model.addPhysicalGroup(1, [lines[3]], 4)
+
+            gmsh.option.setNumber("Mesh.Smoothing", 5)
+            gmsh.option.setNumber("Mesh.RecombineAll", 1)
+            gmsh.option.setNumber("Mesh.Algorithm", 8)
+            gmsh.option.setNumber("Mesh.RecombinationAlgorithm", 2)
+            gmsh.model.mesh.generate(2)
+            gmsh.model.mesh.setOrder(order)
+
+        partitioner = mesh.create_cell_partitioner(mesh.GhostMode.none)
+        msh, _, mt = gmshio.model_to_mesh(
+            gmsh.model, comm, 0, gdim=gdim, partitioner=partitioner)
+        gmsh.finalize()
+
         boundaries = {"left": 4,
                       "right": 2,
                       "bottom": 1,
@@ -296,6 +362,10 @@ class GaussianBump(Problem):
         return fem.Constant(msh, (PETSc.ScalarType(0.0),
                                   PETSc.ScalarType(0.0)))
 
+
+class Square(Problem):
+    def create_mesh(self):
+        return super().create_mesh()
 
 # def u_e(x):
 #     return ufl.as_vector(
