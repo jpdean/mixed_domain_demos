d580af03c74d426292d4c7e71ca8fbb1dc7a6931
Joe Dean <jpd62@cam.ac.uk>
2023-06-01 12:49:36 +0100

    Change params

--- buoyancy_driven_flow.py
+++ buoyancy_driven_flow.py
@@ -239,8 +239,10 @@ def par_print(string):
 
 
 # We define some simulation parameters
-num_time_steps = 1280
-t_end = 5
+# num_time_steps = 1280
+# t_end = 5
+num_time_steps = 5
+t_end = 1
 h = 0.04
 k = 2  # Polynomial degree
 solver_type = hdg_navier_stokes.SolverType.NAVIER_STOKES
