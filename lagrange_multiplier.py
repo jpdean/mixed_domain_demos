46a333462b770c0eae19c204867324164a98f20d
Joe Dean <jpd62@cam.ac.uk>
2025-07-21 16:15:41 +0100

    Update

--- lagrange_multiplier.py
+++ lagrange_multiplier.py
@@ -12,7 +12,7 @@ from ufl import grad, inner, div
 from mpi4py import MPI
 from petsc4py import PETSc
 from utils import norm_L2, one_sided_int_entities
-from dolfinx.fem.petsc import assemble_matrix, assemble_vector, apply_lifting, set_bc
+from dolfinx.fem.petsc import LinearProblem
 from meshing import create_fenics_logo_msh, create_box_with_sphere_msh
 
 
@@ -85,40 +85,19 @@ f = -div(grad(u_e(x_msh)))
 
 L = inner(f, v) * ufl.dx + inner(u_e(x_sm), eta) * ufl.dx
 
-# Define block structure
-a = fem.form(ufl.extract_blocks(a), entity_maps=entity_maps)
-L = fem.form(ufl.extract_blocks(L))
-
-# Assemble matrix
-bcs = [bc]
-A = assemble_matrix(a, bcs=bcs)
-A.assemble()
-
-# Assemble vector
-b = assemble_vector(L, kind=PETSc.Vec.Type.MPI)
-apply_lifting(b, a, bcs=bcs)
-b.ghostUpdate(addv=PETSc.InsertMode.ADD, mode=PETSc.ScatterMode.REVERSE)
-bcs0 = fem.bcs_by_block(fem.extract_function_spaces(L), bcs)
-set_bc(b, bcs0)
-
-# Configure solver
-ksp = PETSc.KSP().create(msh.comm)
-ksp.setOperators(A)
-ksp.setType("preonly")
-ksp.getPC().setType("lu")
-ksp.getPC().setFactorSolverType("superlu_dist")
-
-# Compute solution
-x = A.createVecLeft()
-ksp.solve(b, x)
-
-# Recover solution
 u, lmbda = fem.Function(V), fem.Function(W)
-offset = V.dofmap.index_map.size_local * V.dofmap.index_map_bs
-u.x.array[:offset] = x.array_r[:offset]
-u.x.scatter_forward()
-lmbda.x.array[: (len(x.array_r) - offset)] = x.array_r[offset:]
-lmbda.x.scatter_forward()
+petsc_opts = {"ksp_type": "preonly", "pc_type": "lu", "pc_factor_mat_solver_type": "superlu_dist"}
+problem = LinearProblem(
+    ufl.extract_blocks(a),
+    ufl.extract_blocks(L),
+    u=[u, lmbda],
+    bcs=[bc],
+    kind="mpi",
+    petsc_options_prefix="lagrange_multiplier_",
+    petsc_options=petsc_opts,
+    entity_maps=entity_maps,
+)
+problem.solve()
 
 # Write to file
 with io.VTXWriter(msh.comm, "u.bp", u, "BP4") as f:
